e9f1f4789d0476a806e14e3699bcd5e37d71f171
Savannahjewel <ssteward8761@sdsu.edu>
2025-04-25 17:18:18 -0700

    added light mode

--- js/script.js
+++ js/script.js
@@ -5,3 +5,23 @@ function revealMenu() {
     menu.classList.toggle("revealMenu");
   }
  
+function lightMode () {
+
+    const navDark = document.querySelectorAll('.navlink')
+
+    document.body.classList.toggle("lightMode");
+    document.getElementById("light-toggle").classList.toggle("light-change");
+
+    for (let i = 0; i < navDark.length; i++){
+      navDark[i].classList.toggle("navlink-lightmode");
+    
+    }
+  }
+
+ 
+
+
+if (new Date().getHours () > 18 ) {
+  document.body.classList.toggle("lightMode");
+}
+
