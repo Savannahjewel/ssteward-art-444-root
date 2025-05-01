ffe575f5baee83b534376d3fc0afd3ba256dfb93
Savannahjewel <ssteward8761@sdsu.edu>
2025-05-01 13:38:52 -0700

    buttons

--- js/script.js
+++ js/script.js
@@ -18,10 +18,38 @@ function lightMode () {
     }
   }
 
- 
-
 
 if (new Date().getHours () > 18 ) {
   document.body.classList.toggle("lightMode");
-}
 
+}
+const typewriter = document.querySelector(".typewriter");
+
+typewriter.addEventListener("animationend", (e) => {
+  if (e.animationName === "typing") {
+    typewriter.style.borderRight = "none";
+  }
+});
+
+const images = document.querySelectorAll(".gallery-images");
+
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImage");
+const closeBtn = document.querySelector(".close");
+
+images.forEach(image => {
+  image.addEventListener("click", function() {
+    modal.style.display = "block";  
+    modalImg.src = this.src; 
+  });
+});
+
+closeBtn.addEventListener("click", function() {
+  modal.style.display = "none";  
+});
+
+window.addEventListener("click", function(event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
